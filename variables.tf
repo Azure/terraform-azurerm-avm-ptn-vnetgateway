d1136b4bf8311749fced1ab51313cc83e57f9a69
Luke Taylor <77284962+luke-taylor@users.noreply.github.com>
2023-07-15 16:46:31 +0100

    Update `variables.tf` Descriptions   (#13)
    ## Describe your changes
    
    ## Issue number
    
    #000
    
    ## Checklist before requesting a review
    - [x] The pr title can be used to describe what this pr did in
    `CHANGELOG.md` file
    - [x] I have executed pre-commit on my machine
    - [x] I have passed pr-check on my machine
    
    Thanks for your cooperation!

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "location" {
 
 variable "name" {
   type        = string
-  description = "The name of the virtual network gateway."
+  description = "The name of the Virtual Network Gateway."
 }
 
 variable "resource_group_name" {
@@ -15,7 +15,7 @@ variable "resource_group_name" {
 
 variable "sku" {
   type        = string
-  description = "The SKU (size) of the virtual network gateway."
+  description = "The SKU (size) of the Virtual Network Gateway."
 
   validation {
     condition     = contains(["Basic", "HighPerformance", "Standard", "UltraPerformance", "VpnGw1", "VpnGw2", "VpnGw3", "VpnGw4", "VpnGw5", "VpnGw1AZ", "VpnGw2AZ", "VpnGw3AZ", "VpnGw4AZ", "VpnGw5AZ"], var.sku)
@@ -30,7 +30,7 @@ variable "subnet_address_prefix" {
 
 variable "type" {
   type        = string
-  description = "The type of the virtual network gateway, ExpressRoute or VPN."
+  description = "The type of the Virtual Network Gateway, ExpressRoute or VPN."
 
   validation {
     condition     = contains(["ExpressRoute", "Vpn"], var.type)
@@ -40,7 +40,7 @@ variable "type" {
 
 variable "virtual_network_name" {
   type        = string
-  description = "The name of the virtual network."
+  description = "The name of the Virtual Network."
 }
 
 variable "default_tags" {
@@ -53,7 +53,7 @@ variable "default_tags" {
 variable "edge_zone" {
   type        = string
   default     = null
-  description = "The availability zone of the virtual network gateway. Only supported for AZ SKUs."
+  description = "The availability zone of the Virtual Network Gateway. Only supported for AZ SKUs."
 }
 
 variable "express_route_circuits" {
@@ -70,22 +70,49 @@ variable "express_route_circuits" {
     peering_config = optional(object({
       peering_type                  = string
       vlan_id                       = number
-      ipv4_enabled                  = optional(bool, null)
+      ipv4_enabled                  = optional(bool, true)
       peer_asn                      = optional(number, null)
       primary_peer_address_prefix   = optional(number, null)
       secondary_peer_address_prefix = optional(string, null)
       shared_key                    = optional(string, null)
       route_filter_id               = optional(string, null)
       microsoft_peering_config = optional(object({
-        advertised_communities     = optional(list(string), null)
         advertised_public_prefixes = list(string)
+        advertised_communities     = optional(list(string), null)
         customer_asn               = optional(number, null)
         routing_registry_name      = optional(string, null)
       }), null)
     }), null)
   }))
   default     = {}
-  description = "Express Route circuits configuration."
+  description = <<DESCRIPTION
+Map of Virtual Network Gateway Connections and Peering Configurations to create for existing ExpressRoute circuits.
+
+- `express_route_circuit_id` - (Required) The ID of the ExpressRoute circuit.
+
+- `connection_config` - (Optional) a `connection_config` block as defined below. Used to configure the Virtual Network Gateway Connection between the ExpressRoute Circuit and the Virtual Network Gateway.
+  - `authorization_key` - (Optional) The authorization key for the ExpressRoute Circuit.
+  - `express_route_gateway_bypass` - (Optional) Whether to bypass the ExpressRoute Gateway for data forwarding. 
+  - `name` - (Optional) The name of the Virtual Network Gateway Connection.
+  - `routing_weight` - (Optional) The weight added to routes learned from this Virtual Network Gateway Connection. Defaults to 10.
+  - `shared_key` - (Optional) The shared key for the Virtual Network Gateway Connection.
+  - `tags` - (Optional) A mapping of tags to assign to the resource.
+
+- `peering_config` - (Optional) a `peering_config` block as defined below. Used to configure the ExpressRoute Circuit Peering.
+  - `peering_type` - (Required) The type of the peering. Possible values are AzurePrivatePeering, AzurePublicPeering or MicrosoftPeering.
+  - `vlan_id` - (Required) The VLAN ID for the peering.
+  - `ipv4_enabled` - (Optional) Whether IPv4 is enabled on the peering. Defaults to true.
+  - `peer_asn` - (Optional) The peer ASN.
+  - `primary_peer_address_prefix` - (Optional) The primary address prefix.
+  - `secondary_peer_address_prefix` - (Optional) The secondary address prefix.
+  - `shared_key` - (Optional) The shared key for the peering.
+  - `route_filter_id` - (Optional) The ID of the route filter to apply to the peering.
+  - `microsoft_peering_config` - (Optional) a `microsoft_peering_config` block as defined below. Used to configure the Microsoft Peering.
+    - `advertised_communities` - (Optional) The list of communities to advertise to the Microsoft Peering.
+    - `advertised_public_prefixes` - (Required) The list of public prefixes to advertise to the Microsoft Peering.
+    - `customer_asn` - (Optional) The customer ASN.
+    - `routing_registry_name` - (Optional) The routing registry name.
+DESCRIPTION
   nullable    = false
 
   validation {
@@ -107,7 +134,18 @@ variable "ip_configurations" {
     }), {})
   }))
   default     = {}
-  description = "IP configurations for the virtual network gateway."
+  description = <<DESCRIPTION
+Map of IP Configurations to create for the Virtual Network Gateway.
+
+- `ip_configuration_name` - (Optional) The name of the IP Configuration.
+- `apipa_addresses` - (Optional) The list of APPIPA addresses.
+- `private_ip_address_allocation` - (Optional) The private IP allocation method. Possible values are Static or Dynamic. Defaults to Dynamic.
+- `public_ip` - (Optional) a `public_ip` block as defined below. Used to configure the Public IP Address for the IP Configuration.
+  - `name` - (Optional) The name of the Public IP Address.
+  - `allocation_method` - (Optional) The allocation method of the Public IP Address. Possible values are Static or Dynamic. Defaults to Dynamic.
+  - `sku` - (Optional) The SKU of the Public IP Address. Possible values are Basic or Standard. Defaults to Basic.
+  - `tags` - (Optional) A mapping of tags to assign to the resource.
+DESCRIPTION
   nullable    = false
 }
 
@@ -161,7 +199,50 @@ variable "local_network_gateways" {
     }), null)
   }))
   default     = {}
-  description = "Local network gateways configuration."
+  description = <<DESCRIPTION
+Map of Local Network Gateways and Virtual Network Gateway Connections to create for the Virtual Network Gateway.
+
+- `name` - (Optional) The name of the Local Network Gateway.
+- `address_space` - (Optional) The list of address spaces for the Local Network Gateway.
+- `gateway_fqdn` - (Optional) The gateway FQDN for the Local Network Gateway.
+- `gateway_address` - (Optional) The gateway IP address for the Local Network Gateway.
+- `tags` - (Optional) A mapping of tags to assign to the resource.
+- `bgp_settings` - (Optional) a `bgp_settings` block as defined below. Used to configure the BGP settings for the Local Network Gateway.
+  - `asn` - (Required) The ASN of the Local Network Gateway.
+  - `bgp_peering_address` - (Required) The BGP peering address of the Local Network Gateway.
+  - `peer_weight` - (Optional) The weight added to routes learned from this BGP speaker.
+
+- `connection_config` - (Optional) a `connection_config` block as defined below. Used to configure the Virtual Network Gateway Connection for the Local Network Gateway.
+  - `name` - (Optional) The name of the Virtual Network Gateway Connection.
+  - `type` - (Required) The type of Virtual Network Gateway Connection. Possible values are IPsec or Vnet2Vnet.
+  - `connection_mode` - (Optional) The connection mode.
+  - `connection_protocol` - (Optional) The connection protocol. Possible values are IKEv2 or IKEv1.
+  - `dpd_timeout_seconds` - (Optional) The dead peer detection timeout in seconds.
+  - `egress_nat_rule_ids` - (Optional) The list of egress NAT rule IDs.
+  - `enable_bgp` - (Optional) Whether or not BGP is enabled for this Virtual Network Gateway Connection.
+  - `ingress_nat_rule_ids` - (Optional) The list of ingress NAT rule IDs.
+  - `local_azure_ip_address_enabled` - (Optional) Whether or not the local Azure IP address is enabled.
+  - `peer_virtual_network_gateway_id` - (Optional) The ID of the peer Virtual Network Gateway.
+  - `routing_weight` - (Optional) The routing weight.
+  - `shared_key` - (Optional) The shared key.
+  - `tags` - (Optional) A mapping of tags to assign to the resource.
+  - `use_policy_based_traffic_selectors` - (Optional) Whether or not to use policy based traffic selectors.
+  - `custom_bgp_addresses` - (Optional) a `custom_bgp_addresses` block as defined below. Used to configure the custom BGP addresses for the Virtual Network Gateway Connection.
+    - `primary` - (Required) The primary custom BGP address.
+    - `secondary` - (Required) The secondary custom BGP address.
+  - `ipsec_policy` - (Optional) a `ipsec_policy` block as defined below. Used to configure the IPsec policy for the Virtual Network Gateway Connection.
+    - `dh_group` - (Required) The DH Group used in IKE Phase 1 for initial SA.
+    - `ike_encryption` - (Required) The IKE encryption algorithm (IKE phase 2).
+    - `ike_integrity` - (Required) The IKE integrity algorithm (IKE phase 2).
+    - `ipsec_encryption` - (Required) The IPSec encryption algorithm (IKE phase 1).
+    - `ipsec_integrity` - (Required) The IPSec integrity algorithm (IKE phase 1).
+    - `pfs_group` - (Required) The Pfs Group used in IKE Phase 2 for new child SA.
+    - `sa_datasize` - (Optional) The IPSec Security Association (also called Quick Mode or Phase 2 SA) data size specified in KB for a policy.
+    - `sa_lifetime` - (Optional) The IPSec Security Association (also called Quick Mode or Phase 2 SA) lifetime specified in seconds for a policy.
+  - `traffic_selector_policy` - (Optional) a `traffic_selector_policy` block as defined below. Used to configure the traffic selector policy for the Virtual Network Gateway Connection.
+    - `local_address_prefixes` - (Required) The list of local address prefixes.
+    - `remote_address_prefixes` - (Required) The list of remote address prefixes.
+  DESCRIPTION
   nullable    = false
 
   validation {
@@ -173,34 +254,34 @@ variable "local_network_gateways" {
 variable "route_table_bgp_route_propagation_enabled" {
   type        = bool
   default     = true
-  description = "Whether or not to enable BGP route propagation on the route table."
+  description = "Whether or not to enable BGP route propagation on the Route Table."
   nullable    = false
 }
 
 variable "route_table_creation_enabled" {
   type        = bool
   default     = false
-  description = "Whether or not to create a route table associated with the Virtual Network Gateway Subnet."
+  description = "Whether or not to create a Route Table associated with the Virtual Network Gateway Subnet."
   nullable    = false
 }
 
 variable "route_table_name" {
   type        = string
   default     = null
-  description = "Name of the route table associated with Virtual Network Gateway Subnet."
+  description = "Name of the Route Table associated with Virtual Network Gateway Subnet."
 }
 
 variable "route_table_tags" {
   type        = map(string)
   default     = {}
-  description = "Tags for the route table."
+  description = "Tags for the Route Table."
   nullable    = false
 }
 
 variable "tags" {
   type        = map(string)
   default     = {}
-  description = "Tags to apply to the virtual network gateway."
+  description = "Tags to apply to the Virtual Network Gateway."
   nullable    = false
 }
 
@@ -221,14 +302,14 @@ variable "tracing_tags_prefix" {
 variable "vpn_active_active_enabled" {
   type        = bool
   default     = false
-  description = "Enable active-active mode for the virtual network gateway."
+  description = "Enable active-active mode for the Virtual Network Gateway."
   nullable    = false
 }
 
 variable "vpn_bgp_enabled" {
   type        = bool
   default     = false
-  description = "Enable BGP for the virtual network gateway."
+  description = "Enable BGP for the Virtual Network Gateway."
   nullable    = false
 }
 
@@ -238,7 +319,7 @@ variable "vpn_bgp_settings" {
     peer_weight = optional(number, null)
   })
   default     = null
-  description = "BGP settings for the virtual network gateway."
+  description = "BGP settings for the Virtual Network Gateway."
 }
 
 variable "vpn_generation" {
@@ -255,13 +336,13 @@ variable "vpn_generation" {
 variable "vpn_private_ip_address_enabled" {
   type        = bool
   default     = null
-  description = "Enable private IP address for the virtual network gateway for Virtual Network Gateway Connections. Only supported for AZ SKUs."
+  description = "Enable private IP address for the Virtual Network Gateway for Virtual Network Gateway Connections. Only supported for AZ SKUs."
 }
 
 variable "vpn_type" {
   type        = string
   default     = "RouteBased"
-  description = "The VPN type of the virtual network gateway."
+  description = "The VPN type of the Virtual Network Gateway."
   nullable    = false
 
   validation {
