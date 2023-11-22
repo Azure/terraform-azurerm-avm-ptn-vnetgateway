54ea7e0f8ba8a897b81bf59a526c77b2fa71fdc2
james <53943045+4ppli3d@users.noreply.github.com>
2023-11-22 12:18:19 +0000

    resource group name to locals tf (#31)
    Co-authored-by: Luke Taylor <77284962+luke-taylor@users.noreply.github.com>

--- locals.tf
+++ locals.tf
@@ -55,6 +55,7 @@ locals {
       v.peering,
       {
         express_route_circuit_name = basename(v.id)
+        resource_group_name        = v.resource_group_name
       }
     )
     if v.peering != null
