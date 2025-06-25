d09b32a9766181d663bf957a036dc8b56c78a30b
Jared Holgate <jaredholgate@microsoft.com>
2025-06-25 07:03:07 +0100

    refactor tests

--- examples/vnet-with-subnet/main.tf
+++ examples/vnet-with-subnet/main.tf
@@ -6,19 +6,18 @@ resource "random_id" "id" {
   byte_length = 4
 }
 
-resource "azurerm_resource_group" "rg" {
-  location = "uksouth"
-  name     = "rg-vnetgateway-${random_id.id.hex}"
+locals {
+  location = "swedencentral"
 }
 
-resource "azurerm_resource_group" "rg_two" {
-  location = "uksouth"
-  name     = "rg-vnetgateway-${random_id.id.hex}-02"
+resource "azurerm_resource_group" "rg" {
+  location = local.location
+  name     = "rg-vnetgateway-${random_id.id.hex}"
 }
 
 resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.rg.location
-  name                = "vnet-uksouth-prod"
+  name                = "vnet-prod"
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
 }
@@ -33,7 +32,7 @@ resource "azurerm_subnet" "gateway_subnet" {
 resource "azurerm_public_ip" "public_ip" {
   allocation_method   = "Static"
   location            = azurerm_resource_group.rg.location
-  name                = "pip-uksouth-prod"
+  name                = "pip-${random_id.id.hex}-02"
   resource_group_name = azurerm_resource_group.rg.name
   sku                 = "Standard"
   zones               = ["1", "2", "3"]
@@ -42,8 +41,8 @@ resource "azurerm_public_ip" "public_ip" {
 module "vgw_vpn" {
   source = "../.."
 
-  location = "uksouth"
-  name     = "vgw-vpn-uksouth-prod"
+  location = local.location
+  name     = "vgw-vpn-${random_id.id.hex}"
   ip_configurations = {
     "ip_config_01" = {
       name            = "vnetGatewayConfig01"
@@ -71,7 +70,6 @@ module "vgw_vpn" {
     }
   }
   sku                               = "VpnGw1AZ"
-  subnet_address_prefix             = "10.0.1.0/24"
   subnet_creation_enabled           = false
   type                              = "Vpn"
   virtual_network_gateway_subnet_id = azurerm_subnet.gateway_subnet.id
@@ -82,8 +80,8 @@ module "vgw_vpn" {
 module "vgw_er" {
   source = "../.."
 
-  location = "uksouth"
-  name     = "vgw-ex-uksouth-prod"
+  location = local.location
+  name     = "vgw-ex-${random_id.id.hex}"
   ip_configurations = {
     ip_config_01 = {
       name = "vnetGatewayConfig01"
@@ -94,4 +92,3 @@ module "vgw_er" {
   type                              = "ExpressRoute"
   virtual_network_gateway_subnet_id = azurerm_subnet.gateway_subnet.id
 }
-
